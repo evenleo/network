62cc8b16bb798918aa087770b012ed018068c712
evenleo <1293173298@qq.com>
2019-12-21 18:18:07 +0800

    提交socket客户端和服务器简单交互

--- project1/socket_client.c
+++ project1/socket_client.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define MASSAGE_SIZE 102400
+
+void send_data(int sockfd) {
+    char *query;
+    query = malloc(MASSAGE_SIZE + 1);
+    for (int i = 0; i < MASSAGE_SIZE; ++i) {
+        query[i] = 'a';
+    }
+    query[MASSAGE_SIZE] = '\0';
+
+    const char *cp;
+    cp = query;
+    size_t remaining = strlen(query);
+
+    while (remaining) {
+        int n_written = send(sockfd, cp, remaining, 0);
+        fprintf(stdout, "send into buffer %ld \n", n_written);
+        if (n_written < 0) {
+            error(1, errno, "send failed");
+            return;
+        }
+        remaining -= n_written;
+        cp += n_written;
+    }
+}
 
 int main() {
-    printf("hello client\n");
-    return 0;
+    int sockfd;
+    struct sockaddr_in servaddr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(12400);
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    int connect_rt = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (connect_rt < 0) {
+        error(1, errno, "connect failed");
+    }
+    send_data(sockfd);
+    exit(0);
 }
--- project1/socket_server.c
+++ project1/socket_server.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+/* 从 sockfd 描述符中读取 size 个字节 */
+size_t readn(int fd, void *buffer, size_t size) {
+    char *buffer_pointer = buffer;
+    int length = size;
+    while (length > 0) {
+        int result = read(fd, buffer_pointer, length);
+        if (result < 0) {
+            if (errno == EINTR) 
+                continue;
+            else
+                return -1;
+        } else if (result == 0) 
+            break;
+        length = result;
+        buffer_pointer += result;
+    }
+    return (size - length);  // 返回实际读取的字节数
+}
+
+void read_data(int sockfd) {
+    ssize_t n;
+    char buf[1024];
+    int time = 0;
+    for (;;) {
+        fprintf(stdout, "block in read\n");
+        if ((readn(sockfd, buf, 1024)) == 0) 
+            return;
+        time++;
+        fprintf(stdout, "1K read for %d \n", time);
+        usleep(1000);
+    }
+}
+
 
 int main() {
-    printf("hello server\n");
+    int listenfd, connfd;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(12400);
+
+    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    // listen 的 backlog 为 1024
+    listen(listenfd, 1024);
+
+    for (;;) {
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+        read_data(connfd);
+        close(connfd);
+    }
+
     return 0;
 }
